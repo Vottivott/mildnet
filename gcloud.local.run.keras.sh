7b0f632e5294753a01e438693c427832d0d294f5
Saeed Adel Mehraban <sadelmehraban@gmail.com>
2019-08-04 19:57:01 +0430

    spell bug fix

--- gcloud.local.run.keras.sh
+++ gcloud.local.run.keras.sh
@@ -2,7 +2,7 @@ source settings.cfg
 source $1
 echo $1
 python -m trainer.task --job-dir local_job_dir \
---data-path=$MildNET_DATA_PATH \
+--data-path=$MILDNET_DATA_PATH \
 --model-id=$model_id \
 --loss=$loss \
 --optimizer=$optimizer \
